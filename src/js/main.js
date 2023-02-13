e2fa995d2c4f3ea8e90aef939ecf4b38e9c8180a
Dopamine424 <fomin02@inbox.ru>
2023-02-13 12:30:55 +0500

    block-1-com-5

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,24 @@ ScrollSmoother.create({
     smooth: 1.5,
     effects: true
 });
+
+
+gsap.from('.header', { opacity: 0, duration: 0.7, y: -80});
+
+gsap.to('h1', {opacity: 0, scrollTrigger:{
+    trigger: '.block-1',
+    start: 'top',
+    scrub: true
+}});
+gsap.to('#block-1-cta', {opacity: 0, scrollTrigger:{
+    trigger: '.block-1',
+    start: 'top',
+    scrub: true
+}});
+
+
+document.querySelectorAll('.img-block1').forEach(node => {
+    document.addEventListener('mousemove', e => {
+        node.style.cssText = `--move-x: ${e.clientX}px; --move-y:${e.clientY}px`
+    });
+});
