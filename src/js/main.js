2f2b7dd6b6b6eac1d8355cc84f2c9f38e7c11095
Dopamine424 <fomin02@inbox.ru>
2023-02-19 17:42:41 +0500

    main-com-7

--- src/js/main.js
+++ src/js/main.js
@@ -22,20 +22,19 @@ const tl = gsap.timeline({defaults: {duration: .7}});
 
 
 
-tl.from('.header', { opacity: 0, duration: 0.7, y: -80});
+tl.from('.header', { opacity: 0, y: -80})
+    .from('.menu-active',{opacity: 0} )
+    //.from('h1', {opacity: 0});
 
-tl.from('.menu-active',{opacity: 0, duration: 0.7} );
-
-
-
-tl.from('h1', { opacity: 0, display: 'none'})
-.add(h1Animation);
+tl.add(h1Animation);
 
 tl.to('#block-1-cta', {opacity: 0, display: 'none', scrollTrigger:{
     trigger: '.block-1',
     start: 'top',
     scrub: true
 }});
+
+gsap.from('.menu-active', {opacity: 0, duration: 5});
 
 
 document.querySelectorAll('.img-block1').forEach(node => {
@@ -60,6 +59,7 @@ document.querySelectorAll('.cursor').forEach(node => {
 $(document).ready(function() {
     $('.burger').click(function(event){
         $('.menu').toggleClass('menu-active');
+        $('.body').toggleClass('hide');
     });
 });
 
@@ -68,16 +68,147 @@ $(document).ready(function() {
     $('.collections').mouseover(function(event){
         $('.ul2').css('opacity','1');
         $('.ul2').css('pointer-events','auto');
+
         $(this).css('color', 'white');
         $(this).addClass('a-hover');
+
+        $('.musems').removeClass('a-hover');
+        $('.musems').css('color', '#8F969E');
+
+        $('.vistv').removeClass('a-hover');
+        $('.vistv').css('color', '#8F969E');
+
+        $('.posit').removeClass('a-hover');
+        $('.posit').css('color', '#8F969E');
+
+        $('.hud').removeClass('a-hover');
+        $('.hud').css('color', '#8F969E');
+
+        $('.contacts').removeClass('a-hover');
+        $('.contacts').css('color', '#8F969E');
     });
 
 
     $('.musems').mouseover(function(event){
         $('.ul2').css('opacity','0');
         $('.ul2').css('pointer-events','none');
+
+        $(this).css('color', 'white');
+        $(this).addClass('a-hover');
+
         $('.collections').removeClass('a-hover');
         $('.collections').css('color', '#8F969E');
+
+        $('.vistv').removeClass('a-hover');
+        $('.vistv').css('color', '#8F969E');
+ 
+        $('.posit').removeClass('a-hover');
+        $('.posit').css('color', '#8F969E');
+
+        $('.hud').removeClass('a-hover');
+        $('.hud').css('color', '#8F969E');
+
+        $('.contacts').removeClass('a-hover');
+        $('.contacts').css('color', '#8F969E');
+        
+    });
+
+
+    $('.vistv').mouseover(function(event){
+        $('.ul2').css('opacity','0');
+        $('.ul2').css('pointer-events','none');
+
+        $(this).css('color', 'white');
+        $(this).addClass('a-hover');
+
+        $('.collections').removeClass('a-hover');
+        $('.collections').css('color', '#8F969E');
+
+        $('.musems').removeClass('a-hover');
+        $('.musems').css('color', '#8F969E');
+
+        $('.posit').removeClass('a-hover');
+        $('.posit').css('color', '#8F969E');
+
+        $('.hud').removeClass('a-hover');
+        $('.hud').css('color', '#8F969E');
+
+        $('.contacts').removeClass('a-hover');
+        $('.contacts').css('color', '#8F969E');
+ 
+    });
+
+    $('.posit').mouseover(function(event){
+        $('.ul2').css('opacity','0');
+        $('.ul2').css('pointer-events','none');
+
+        $(this).css('color', 'white');
+        $(this).addClass('a-hover');
+
+        $('.collections').removeClass('a-hover');
+        $('.collections').css('color', '#8F969E');
+
+        $('.musems').removeClass('a-hover');
+        $('.musems').css('color', '#8F969E');
+
+        $('.vistv').removeClass('a-hover');
+        $('.vistv').css('color', '#8F969E');
+
+        $('.hud').removeClass('a-hover');
+        $('.hud').css('color', '#8F969E');
+
+        $('.contacts').removeClass('a-hover');
+        $('.contacts').css('color', '#8F969E');
+ 
+    });
+
+    $('.hud').mouseover(function(event){
+        $('.ul2').css('opacity','0');
+        $('.ul2').css('pointer-events','none');
+
+        $(this).css('color', 'white');
+        $(this).addClass('a-hover');
+
+        $('.collections').removeClass('a-hover');
+        $('.collections').css('color', '#8F969E');
+
+        $('.musems').removeClass('a-hover');
+        $('.musems').css('color', '#8F969E');
+
+        $('.vistv').removeClass('a-hover');
+        $('.vistv').css('color', '#8F969E');
+
+        $('.posit').removeClass('a-hover');
+        $('.posit').css('color', '#8F969E');
+
+        $('.contacts').removeClass('a-hover');
+        $('.contacts').css('color', '#8F969E');
+ 
+    });
+
+    $('.contacts').mouseover(function(event){
+        $('.ul2').css('opacity','0');
+        $('.ul2').css('pointer-events','none');
+
+        $(this).css('color', 'white');
+        $(this).addClass('a-hover');
+
+        $('.collections').removeClass('a-hover');
+        $('.collections').css('color', '#8F969E');
+
+        $('.musems').removeClass('a-hover');
+        $('.musems').css('color', '#8F969E');
+
+        $('.vistv').removeClass('a-hover');
+        $('.vistv').css('color', '#8F969E');
+
+        $('.posit').removeClass('a-hover');
+        $('.posit').css('color', '#8F969E');
+
+        
+        $('.hud').removeClass('a-hover');
+        $('.hud').css('color', '#8F969E');
+ 
     });
 
 
