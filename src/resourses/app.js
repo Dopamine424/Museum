62f27e4ddd94e544a763c768539ea0a3701467b0
Alexandr <fomin02@inbox.ru>
2023-04-20 12:24:31 +0500

    com-col-fix

--- src/resourses/app.js
+++ src/resourses/app.js
@@ -1,21 +1,19 @@
 window.onload = function() {
-
 	setTimeout(function() {
-
-		document.body.classList.add('loaded')
-
-		if (window.matchMedia('(min-width: 992px)').matches) { // If not mobile
-
-			Draggable.create('.gallery', {
-				bounds: 'body',
-				inertia: true
-			})
-			
-		}
-
+	document.body.classList.add('loaded')
+	if (window.matchMedia('(min-width: 992px)').matches) { // If not mobile
+	Draggable.create('.gallery', {
+	bounds: 'body',
+	inertia: true
+	})
+	
+	}
+	// Обработчик клика на элементы галереи
+	$('.gallery__item').click(function() {
+	window.location.href = 'col.html';
+	});
 	}, 200)
-
-}
+	}
 document.querySelectorAll('.cursor').forEach(node => {
     document.addEventListener('mousemove', e => {
         node.style.cssText = `--move-x: ${e.clientX}px; --move-y: ${e.clientY}px;` 
